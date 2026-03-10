ec1f4ca4b0363a156e30e218607074cc9be22aab
Vaibhav Rakheja <vaibhavrakheja11@gmail.com>
2026-03-09 20:51:36 -0400

    first deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "Arial"
+    }}>
+      <h1>PAVAN</h1>
+    </div>
+  );
+}
