52461ef49c545fed1e09545ab61d6b4776548904
vaibhavrakheja11 <vaibhavrakheja11@gmail.com>
2026-03-11 04:30:26 -0400

    Revert "Frontend first commit"

--- my-app/src/views/careers/TagBadge.jsx
+++
@@ -1,8 +0,0 @@
-// src/views/careers/TagBadge.jsx
-// Pure UI component for displaying a tag badge
-
-import React from 'react';
-
-export const TagBadge = ({ tag }) => (
-    <span className="career-tag">{tag}</span>
-);
--- my-app/src/views/careers/index.js
+++
@@ -1,6 +0,0 @@
-// src/views/careers/index.js
-// Barrel export for career view components
-
-export { JobCard } from './JobCard';
-export { JobDetail } from './JobDetail';
-export { TagBadge } from './TagBadge';
